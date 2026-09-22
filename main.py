a64f79c209a34681906220eebf4118aaacee9249
Hancheng Guo <silence6v6@gmail.com>
2026-09-22 13:40:31 +0800

    refactor training configs

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from app.application_entry import ApplicationEntry
 
 
-# with ApplicationEntry("unitree_go1_isaac_sim_test") as application:
+# app_name = "unitree_go1_isaac_cuda_test"
+# app_name = "unitree_go1_isaac_cuda_velocity"
+app_name = "unitree_go1_mujoco_cpu_velocity"
+train_time = None
+device = None
 
-with ApplicationEntry("unitree_go1_isaac_sim") as application:
-
-# with ApplicationEntry(
-#     "unitree_go1_isaac_sim",
-#     train_time="2026-09-15_23-11-41",
-# ) as application:
-    
+with ApplicationEntry(app_name, train_time, device) as application:
     application.train()
     application.save()
     application.play(num_steps=500, formats="gif")
-    input("Press...")
+    input("Press...\n")
